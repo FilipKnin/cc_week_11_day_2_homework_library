3e3e81b9b192a927f3f43026e06fbdd70bc9d208
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-09 16:50:58 +0100

    Initial commit. Classes of Book and Library created. Book.hasTitle/Author/genre created and tested

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+    Book book;
+    Library library;
+
+    @Before
+    public void before() {
+        book = new Book("Title1", "Author1", "sci-fi");
+        library = new Library();
+
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("Title1", book.getTitle());
+    }
+
+    @Test
+    public void hasAuthor() {
+        assertEquals("Author1", book.getAuthor());
+    }
+
+    @Test
+    public void hasGenre() {
+        assertEquals("sci-fi", book.getGenre());
+    }
+
+}
